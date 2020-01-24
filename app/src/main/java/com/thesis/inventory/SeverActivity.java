4758fd30382a30acd2424c63801bef51e024807d
Dancharles Okoto <dancharles.okoto@gmail.com>
2020-01-24 13:40:06 +0100

    new upload

--- app/src/main/java/com/thesis/inventory/SeverActivity.java
+++ app/src/main/java/com/thesis/inventory/SeverActivity.java
@@ -3,12 +3,16 @@ package com.thesis.inventory;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.VolleyError;
@@ -22,6 +26,7 @@ import org.json.JSONObject;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.List;
 
 import okhttp3.Call;
@@ -36,9 +41,10 @@ public class SeverActivity extends AppCompatActivity {
 
     private TextView resultTextView, prodname, prodquan, ptreddate, prodes;
     private static final String TAG = SeverActivity.class.getSimpleName();
-    String dId, dTitle, dDes, dBar, dQuantity, dPrice, dProno;
+    String dId, dTitle, dDes, dBar, dQuantity, dPrice, dProno,  c, d, e;
 
-    DatePicker picker;
+    DatePickerDialog picker;
+    EditText eText;
     Button btnGet;
 
     @RequiresApi(api = Build.VERSION_CODES.O)
@@ -52,7 +58,33 @@ public class SeverActivity extends AppCompatActivity {
         prodquan = findViewById(R.id.stock);
         prodes = findViewById(R.id.itemdes);
 
-        picker=(DatePicker)findViewById(R.id.datePicker1);
+        eText=(EditText) findViewById(R.id.editText1);
+        eText.setInputType(InputType.TYPE_NULL);
+
+        eText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar cldr = Calendar.getInstance();
+                int day = cldr.get(Calendar.DAY_OF_MONTH);
+                int month = cldr.get(Calendar.MONTH);
+                int year = cldr.get(Calendar.YEAR);
+                // date picker dialog
+
+                picker = new DatePickerDialog(SeverActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                c = String.valueOf(monthOfYear);
+                                d = String.valueOf(dayOfMonth);
+                                e = String.valueOf(year);
+                                eText.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                            }
+                        }, year, month, day);
+
+                picker.show();
+            }
+        });
+
         btnGet=(Button)findViewById(R.id.btnpredict);
 
         Bundle bundle = getIntent().getExtras();
@@ -77,9 +109,14 @@ public class SeverActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
+                if (c.isEmpty() || d.isEmpty() || e.isEmpty()){
 
-                String a="1",b=dProno,c=String.valueOf(picker.getMonth()),d= String.valueOf(picker.getDayOfMonth()), e=String.valueOf(picker.getYear());
+                    Toast.makeText(SeverActivity.this, "Date to predict is not set", Toast.LENGTH_SHORT).show();
 
+                    return;
+                }
+
+                String a="1",b=dProno;
                 List<String> list = Arrays.asList(a,b,c,d,e);
 
                 String result = String.join(",",list);
